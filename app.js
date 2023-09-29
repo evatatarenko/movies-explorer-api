186895ce7afa0091da7547d6c21cd12377000c31
evatatarenko <evatatarenko@yandex.ru>
2023-09-29 19:04:18 +0300

    change paths

--- app.js
+++ app.js
@@ -3,19 +3,21 @@ const express = require('express');
 const helmet = require('helmet');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
-const limiter = require('../../movies-explorer-api/middlewares/rateLimit');
-const errorProcesser = require('../../movies-explorer-api/middlewares/errorProcessing');
-const { requestLogger, errorLogger } = require('../../movies-explorer-api/middlewares/logger');
-const { MONGO_URL_DEV } = require('../../movies-explorer-api/utils/constants');
-const { routes } = require('../../movies-explorer-api/routes');
+const limiter = require('./middlewares/rateLimit');
+const errorProcesser = require('./middlewares/errorProcessing');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const { MONGO_URL_DEV } = require('./utils/constants');
+const { routes } = require('./routes');
 const cors = require('./middlewares/cors');
 
 const { PORT = 3000, NODE_ENV, MONGO_URL } = process.env;
 const app = express();
 
 mongoose.connect(NODE_ENV === 'production' ? MONGO_URL : MONGO_URL_DEV);
-app.use(cors);
+
 app.use(requestLogger);
+app.use(cors);
+
 if (NODE_ENV === 'production') {
   app.use(helmet());
   app.use(limiter);
